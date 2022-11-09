78be1ed800676db13af9f0ae4f76bb83fac10dd2
Naveen6489 <102216712+Naveen6489@users.noreply.github.com>
2022-11-09 17:29:06 +0530

    import java.util.*; class main{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         float pie=3.14f,n;         n=pie*a*a;         System.out.println(n);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        float pie=3.14f,n;
+        n=pie*a*a;
+        System.out.println(n);
+    }
+}
