6043b8b459251f310e71d2bec839f10522afbe5c
Naveen6489 <102216712+Naveen6489@users.noreply.github.com>
2022-11-09 17:20:21 +0530

    import java.util.*; class main{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);
            String c1=sc.nextLine();
            for(int i=0;i<c1.length();i++){
                char ch=c1.charAt(i);
            System.out.print(ch+"");
            }
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        
+        String c1=sc.nextLine();
+        for(int i=0;i<c1.length();i++){
+            char ch=c1.charAt(i);
+        System.out.print(ch+"");
+        }
+    }
+}
